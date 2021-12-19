3f33d13342c98ddd21b43c1a045b91e35f75bb58
EduardsZ <eduards.zvaigzne@gmail.com>
2021-12-19 23:00:48 +0200

    Added feels like field

--- js/main.js
+++ js/main.js
@@ -15,11 +15,12 @@ function mm(id = defCity) {
 }
 
 function rw(data) {
+  console.log(data);
   if(wDiv && data.cod == 200){
     let out = '<div class="weathme">'
     out += `<h2>${data.city.name}</h2><div id="weathmes">`
     data.list.forEach(element => {
-      out += `<div class="wethtile"><h3>${element.dt_txt.substr(0, element.dt_txt.length - 3)}</h3><div>Temp: ${(element.main.temp - kelvins).toFixed(1)} &deg;C</div><div><img src="http://openweathermap.org/img/wn/${element.weather[0].icon}.png"></div> <div>${element.weather[0].main}</div><div>Wind: ${element.wind.speed} m/s <span style="transform: rotate(${element.wind.deg}deg);display: inline-block">&darr;</span></div></div>`
+      out += `<div class="wethtile"><h3>${element.dt_txt.substr(0, element.dt_txt.length - 3)}</h3><div>Temp: ${(element.main.temp - kelvins).toFixed(1)} &deg;C</div><div>Feels like: ${(element.main.feels_like - kelvins).toFixed(1)} &deg;C</div><div><img src="http://openweathermap.org/img/wn/${element.weather[0].icon}.png"></div> <div>${element.weather[0].main}</div><div>Wind: ${element.wind.speed} m/s <span style="transform: rotate(${element.wind.deg}deg);display: inline-block">&darr;</span></div></div>`
     });
     out += '</div></div>'
     wDiv.innerHTML = out
